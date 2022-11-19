a3f2bf6f646794315a27f1dcc40e47fc8d196ecc
Eric Saxby <sax@livinginthepast.org>
2022-11-19 13:59:42 -0800

    Update flags: -U -S -P vs -u -s -p

--- src/cli.rs
+++ src/cli.rs
@@ -3,26 +3,27 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct CLI {
-  #[clap(long, short, default_value = "127.0.0.1")]
+  #[clap(long, short = 'S', default_value = "127.0.0.1")]
   pub server: String,
 
-  #[clap(long, default_value_t = 1433)]
+  #[clap(long, short, default_value_t = 1433)]
   pub port: u16,
 
   #[clap(long, short)]
   pub database: String,
 
-  #[clap(long, short, default_value = "sa")]
+  #[clap(long, short = 'U', default_value = "sa")]
   pub username: String,
 
   #[arg(default_value = "")]
   pub query: Vec<String>,
 
-  /// Ask for the password
-  #[clap(long, short, default_value_t = false)]
+  /// Ask for the password. If not specified, then AZURE_SQL_PASSWORD
+  /// must be set.
+  #[clap(long, short = 'P', default_value_t = false)]
   pub password: bool,
 
-  /// Disable encryption, for instance when connecting on localhost
+  /// Disable encryption, for instance when connecting on localhost.
   #[clap(long, default_value_t = false)]
   pub no_encryption: bool,
 
